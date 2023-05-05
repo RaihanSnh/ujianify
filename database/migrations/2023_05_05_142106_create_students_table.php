0dacbee8b91229f8954259319707de25e1a47f8e
AkmalFairuz <akmalf475@gmail.com>
2023-05-05 22:04:23 +0700

    Add migrations

---
+++ database/migrations/2023_05_05_142106_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->foreignID('user_id')->primary()->references('id')->on('users');
+
+            $table->string('external_id')->index();
+            $table->string('full_name');
+
+            $table->foreignID('major_id')->references('id')->on('majors');
+            $table->foreignID('classroom_id')->references('id')->on('classroom');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
