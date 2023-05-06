6395157d7d0f7e3512a7fee2ec089fe3bf546d9c
AkmalFairuz <akmalf475@gmail.com>
2023-05-06 15:12:44 +0700

    Add score table

---
+++ database/migrations/2023_05_06_081055_create_scores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->foreignId('student_id')->references('user_id')->on('students');
+            $table->foreignId('subject_id')->references('id')->on('subjects');
+
+            $table->integer('score');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scores');
+    }
+};
