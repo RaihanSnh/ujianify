1b12ae42adb430b02669e0b8d42736a4b3e136eb
AkmalFairuz <akmalf475@gmail.com>
2023-05-06 19:52:30 +0700

    Remove JWT

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -5,22 +5,11 @@ declare(strict_types=1);
 namespace App\Services\Auth;
 
 use App\Models\User;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
 use Illuminate\Http\Request;
-use function time;
 
 class AuthService{
 
-    private string $key;
-    private int $expirationTime = 60 * 60 * 24; // 60 seconds * 60 minutes * 24 hours = 1 day
-    private string $algorithm = 'HS256';
-
-    public function __construct() {
-        $this->key = env('JWT_SECRET_KEY');
-    }
-
-    public function set(Request $request, User $user) {
+    public function set(Request $request, User $user): void{
         $request->session()->put('authenticated_user', [
             'user_id' => $user->id,
             'name' => $user->name,
