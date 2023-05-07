9ab3fefcb955134083a614e1812eeed202134f3b
RaihanSnh <nathahamzahraihansatya@gmail.com>
2023-05-07 19:23:23 +0700

    push

--- resources/js/toggle_password_visibility.js
+++ resources/js/toggle_password_visibility.js
@@ -1,7 +1,9 @@
 function onTogglePasswordVisibility(inputID, buttonIconID) {
+    // console.log('inputID:', inputID);
+    // console.log('buttonIconID:', buttonIconID);
     const passwordInput = document.getElementById(inputID);
     const toggleIcon = document.getElementById(buttonIconID);
     const show = passwordInput.type === 'password';
-    toggleIcon.innerText = show ? 'visibility' : 'visibility_off';
+    toggleIcon.innerHTML = show ? 'visibility' : 'visibility_off';
     passwordInput.type = show ? 'text' : 'password';
 }
--- resources/views/components/input/password.blade.php
+++ resources/views/components/input/password.blade.php
@@ -1,6 +1,6 @@
 <div class="relative">
-    <input type="password" name="{{ $name }}" placeholder="{{ $placeholder }}" class="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:border-blue-300 focus:outline-none focus:shadow-outline">
-    <button type="button" onclick="togglePassword()" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-        <i class="material-icons">visibility</i>
+    <input type="password" id="{{ $name }}" name="{{ $name }}" placeholder="{{ $placeholder }}" class="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:border-blue-300 focus:outline-none focus:shadow-outline">
+    <button type="button" id="{{ $name }}-button" onclick="onTogglePasswordVisibility('{{ $name }}', '{{ $name }}-button-icon')" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"/>
+        <i id="{{ $name }}-button-icon" class="material-icons">visibility</i>
     </button>
 </div>
