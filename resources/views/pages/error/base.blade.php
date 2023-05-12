b7d7fd54f4ccfe91a655d5be4fe14b92ef53e78c
AkmalFairuz <akmalf475@gmail.com>
2023-05-12 19:35:26 +0700

    fix

--- resources/views/pages/error/base.blade.php
+++ resources/views/pages/error/base.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     <div class="flex flex-col justify-center items-center h-[100vh] w-full">
         <div class="flex flex-row justify-center items-center w-full">
-            <div class="flex flex-col w-full text-2xl font-bold">
+            <div class="flex flex-col text-2xl font-bold rounded-lg border-gray-200 border py-4 px-6 shadow-md mx-2">
                 @yield('error')
             </div>
         </div>
--- resources/views/pages/error/forbidden.blade.php
+++ resources/views/pages/error/forbidden.blade.php
@@ -1,12 +1,6 @@
 @extends('pages.error.base')
 
 @section('error')
-    <div class="flex flex-col gap-2">
-        <div class="flex w-full justify-center">
-            <div class="flex flex-col gap-4">
-                <div>403 Forbidden</div>
-                <div class="font-normal text-lg">You do not have permission to access this path!</div>
-            </div>
-        </div>
-    </div>
+    <div>403 Forbidden</div>
+    <div class="font-normal text-lg">You do not have permission to access this path!</div>
 @endsection
