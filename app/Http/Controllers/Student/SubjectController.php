fd3c1fd91105445bfcd9c4b1abe581eb69d630df
AkmalFairuz <akmalf475@gmail.com>
2023-05-15 23:26:29 +0700

    fix

--- app/Http/Controllers/Student/SubjectController.php
+++ app/Http/Controllers/Student/SubjectController.php
@@ -80,7 +80,7 @@ class SubjectController extends Controller
 
 	public function submit(Subject $subject, Request $request)
 	{
-		if($subject->starts_at->greatherThan(Carbon::now())) {
+		if($subject->starts_at->greaterThan(Carbon::now())) {
 			return view('pages.error.simple', ['err' => 'Subject not started yet']);
 		}
 		if (Score::query()->where('student_id', '=', $request->user()->getUserId())->where('subject_id', '=', $subject->id)->whereNotNull('submitted_at')->first() !== null) {
