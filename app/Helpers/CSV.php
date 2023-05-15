e75e03cdda6e7a0a1eb0e82faeb405240cd20626
AkmalFairuz <akmalf475@gmail.com>
2023-05-15 21:43:17 +0700

    Fix

--- app/Helpers/CSV.php
+++ app/Helpers/CSV.php
@@ -4,37 +4,26 @@ declare(strict_types=1);
 
 namespace App\Helpers;
 
-use function array_map;
-use function implode;
-use function is_string;
-use function str_contains;
-use function str_replace;
+use Illuminate\Support\Str;
+use Symfony\Component\Filesystem\Path;
+use function fclose;
+use function file_get_contents;
+use function fopen;
+use function fputcsv;
+use function sys_get_temp_dir;
+use function unlink;
 
 class CSV{
 
 	public static function create(array $data) : string{
-		$ret = '';
-		foreach ($data as $row) {
-			$shouldEscape = false;
-			foreach ($row as $value) {
-				if(!is_string($value)) {
-					$value = (string) $value;
-				}
-				if (str_contains($value, ',') || str_contains($value, '"')) {
-					$shouldEscape = true;
-					break;
-				}
-			}
-			if ($shouldEscape) {
-				$escapedRow = array_map(function ($value) {
-					return '"' . str_replace('"', '""', $value) . '"';
-				}, $row);
-				$line = implode(',', $escapedRow);
-			} else {
-				$line = implode(',', $row);
-			}
-			$ret .= $line . "\n";
+		$path = Path::join(sys_get_temp_dir(), Str::random(32)) . '.csv';
+		$f = fopen($path, 'w');
+		foreach($data as $x) {
+			fputcsv($f, $x);
 		}
+		@fclose($f);
+		$ret = file_get_contents($path);
+		@unlink($path);
 		return $ret;
 	}
 }
